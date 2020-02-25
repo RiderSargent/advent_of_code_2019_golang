a67713df102282ba095f612f1e8592c766f02c3f
Rider Sargent <rider.sargent@gmail.com>
2020-02-25 17:30:59 +0000

    Day 1, part 1.

--- day01/main.go
+++ day01/main.go
@@ -1,4 +1,44 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// Part 1: 3303995
+// Part 2: 4953118
 func main() {
+	modules := getInput()
+	totalFuel := 0
+
+	for _, mass := range modules {
+		totalFuel += mass/3 - 2
+	}
+
+	fmt.Print("Part 1: ")
+	fmt.Println(totalFuel)
+}
+
+func getInput() []int {
+	file, err := os.Open("input")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+
+	var numbers []int
+	for scanner.Scan() {
+		result, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+		numbers = append(numbers, result)
+	}
+
+	return numbers
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RiderSargent/advent_of_code_2019_golang
+
+go 1.13
