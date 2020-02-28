8bd0866f7792476eec4468eb884d92ab3e716fcf
Rider Sargent <rider.sargent@gmail.com>
2020-02-28 15:50:00 +0000

    Day 3, part 2.

--- day03/main.go
+++ day03/main.go
@@ -13,27 +13,61 @@ import (
 func main() {
 	routes := getInput("day03/input")
 
-	fmt.Println("Part 1:", getClosestIntersection(routes))
+	points1 := toPoints(routes[0])
+	points2 := toPoints(routes[1])
+	intersections := intersections(points1, points2)
+
+	fmt.Println("Part 1:", closestIntersection(intersections))
+	fmt.Println("Part 2:", shortestPath(points1, points2, intersections))
 }
 
-func getClosestIntersection(routes []string) int {
-	points1 := getUniquePoints(routes[0])
-	points2 := getUniquePoints(routes[1])
-	points := append(points1, points2...)
+func intersections(p1, p2 []point) []point {
+	p1 = unique(p1)
+	p2 = unique(p2)
+	points := append(p1, p2...)
+	var intersections []point
 
-	pointMap := pointMap(points)
+	pointMap := make(map[point]int)
+	for _, p := range points {
+		pointMap[p]++
+		if pointMap[p] > 1 {
+			intersections = append(intersections, p)
+		}
+	}
 
+	return intersections[1:] // don't inlude (0, 0)
+}
+
+func closestIntersection(points []point) int {
 	min := math.MaxInt32
-	for point, count := range pointMap {
-		if count > 1 {
-			d := manhattanDistance(point)
-			if d < min {
-				min = d
-			}
+	for _, p := range points {
+		md := manhattanDistance(p)
+		if md < min {
+			min = md
 		}
 	}
 
 	return min
+}
+
+func shortestPath(p1, p2, isects []point) int {
+	min := math.MaxInt32
+	for _, intersection := range isects {
+		totalSteps := indexOf(intersection, p1) + indexOf(intersection, p2)
+		if totalSteps < min {
+			min = totalSteps
+		}
+	}
+	return min
+}
+
+func indexOf(p point, list []point) int {
+	for i, v := range list {
+		if v == p {
+			return i
+		}
+	}
+	return -1
 }
 
 func pointMap(points []point) map[point]int {
@@ -46,7 +80,7 @@ func pointMap(points []point) map[point]int {
 	return m
 }
 
-func getUniquePoints(route string) []point {
+func toPoints(route string) []point {
 	points := make([]point, 1, 155000)
 	for _, segment := range strings.Split(route, ",") {
 		last := points[len(points)-1]
@@ -73,7 +107,7 @@ func getUniquePoints(route string) []point {
 			panic("illegal direction")
 		}
 	}
-	return unique(points)
+	return points
 }
 
 func unique(points []point) []point {
